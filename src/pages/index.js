54dee8fc72275fede1dc725e4bd0fc9af52d63f9
VinodhPIT <vinodh.mn@pitsolutions.com>
2024-06-04 19:19:35 +0530

    wdwd

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,25 @@
-import React, { useState ,useRef} from "react";
+import React, { useState, useEffect, useRef } from "react";
+
+
+
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
 import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
 
+
+
 const ContactForm = () => {
-  const options = ["one", "two", "three"];
+  const [loader, setLoader] = useState(false);
+
+
+
 
 
   const emailInputRef = useRef(null);
-  const phoneInputRef = useRef(null); // New ref for phone input field
+  const phoneInputRef = useRef(null);
+
+
 
   const handleCountryFocus = () => {
     if (emailInputRef.current || phoneInputRef.current) {
@@ -17,26 +28,51 @@ const ContactForm = () => {
     }
   };
 
-
-  const handleCountryChange = (selectedOptions) => {
-    console.log(selectedOptions ,"selectedOptions")
+  const handleSubmit = async (values) => {
+  
   };
+
+
+  
+
+  const validationSchema = Yup.object().shape({
+    email: Yup.string()
+      .email('jkhk').matches(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, "jj"),
+      phone: Yup.string().matches(
+      /^[0-9()+\- ]+$/,
+     'nlklk'
+    ),
+  });
+
+  useEffect(() => {
+    //fetchCountryCodelists();
+  }, []);
+
+  const options = [
+    'one', 'two', 'three'
+  ];
 
   return (
     <>
-      <div className="full_col_block h_126_vh m_h_118_vh">
+      <div className="full_col_block h_126_vh m_h_60_vh">
         <div className="container">
           <div className="row">
-            <div className="col-md-12 align_content">
-              <section className="request_landing_content mb_90">
+            <div className="col-md-12 align_content m_align_content">
+              <section className="request_landing_content mb_90 m_mb_0">
                 <div className="request_landing_content_col">
-                  <h2>cdcdc</h2>
+                  <h2>dwdwd</h2>
                   <div className="request_contact_form">
-                    <Formik initialValues={{ email: "cd", phone: "cd" }}>
+                    <Formik
+                      initialValues={{ email: "", phone: '' }}
+                      validationSchema={validationSchema}
+                      onSubmit={handleSubmit}
+                    >
                       {({ values, errors, touched }) => (
                         <Form className="form_floating">
                           <div className="form_block">
-                            <label htmlFor="email">ddcdc</label>
+                            <label htmlFor="email">
+                             efewfe
+                            </label>
                             <Field
                               type="email"
                               id="email"
@@ -44,7 +80,6 @@ const ContactForm = () => {
                               className="form_control"
                               placeholder="Your e-mail"
                               innerRef={emailInputRef} // Assign the ref
-
                             />
                             <ErrorMessage
                               name="email"
@@ -52,23 +87,34 @@ const ContactForm = () => {
                               className="error"
                             />
                           </div>
-                          <div className="form_block mt_10">
-                            <label htmlFor="phone">cdcd</label>
+                          <div className="form_block">
+                            <label htmlFor="phone">
+                             eded
+                            </label>
+
                             <div style={{ display: "flex", gap: "8px" }}>
                               <Dropdown
                                 options={options}
-                                value={"c"}
-                                onChange={handleCountryChange}
+                                value={'1'}
+                              
                                 onFocus={handleCountryFocus} // Blur email input field on focus
+                              >
+                                {options.map((option) => (
+                                  <option key={option.key} value={option.value}>
+                                    {option.label}
+                                  </option>
+                                ))}
+                              </Dropdown>
 
-                              />
                               <Field
-                                type="text"
+                                type="number"
                                 id="phone"
                                 name="phone"
                                 className="form_control"
                                 placeholder="Your phone number"
-                                innerRef={phoneInputRef} // Assign the ref
+                                innerRef={phoneInputRef}
+                              
+                                
                               />
                             </div>
                             <ErrorMessage
@@ -77,12 +123,39 @@ const ContactForm = () => {
                               className="error"
                             />
                           </div>
-                          <button
-                            type="submit"
-                            className="btn_secondary btn_cutom_40 mt_15 pull_right align_self_end bdr_rad_4"
-                          >
-                            dccd
-                          </button>
+
+                          <div className="request_ref_btn rqst_btn_bottom request_mob_fixed m_gap_16 m_pb_15 m_pt_10">
+                        
+                            <button
+                              className="btn_defult_base pull_right align_self_end m_w_50pc"
+                              disabled={values.email === ""}
+                              type="submit"
+                            >
+                           ded
+
+                              {loader && (
+                                <span
+                                  className="spinner-border spinner-border-sm"
+                                  aria-hidden="true"
+                                ></span>
+                              )}
+                            </button>
+                          </div>
+
+                          {/* {values.email && (
+                            <button
+                              type="submit"
+                              className="btn_defult_base mt_15 pull_right align_self_end"
+                            >
+                              {t("common:next")}
+                              {loader && (
+                                <span
+                                  className="spinner-border spinner-border-sm"
+                                  aria-hidden="true"
+                                ></span>
+                              )}
+                            </button>
+                          )} */}
                         </Form>
                       )}
                     </Formik>
